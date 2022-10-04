3dd3d0d5b2bac81aa7e31f4aeb6757bd0681debe
dsjk3172 <dsjk3172@naver.com>
2022-10-04 17:30:06 +0900

    3장 구현 완료

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -1,44 +1,10 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
-from .forms import QuestionForm, AnswerForm
-from .models import Question
-from django.contrib import messages
-
-
-def index(request):
-    page = request.GET.get('page', '1')  # 페이지
-    question_list = Question.objects.order_by('-create_date')
-    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
-    page_obj = paginator.get_page(page)
-    context = {'question_list': page_obj}
-    return render(request, 'pybo/question_list.html', context)
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
-    return render(request, 'pybo/question_detail.html', context)
-
-
-@login_required(login_url='common:login')
-def answer_create(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    if request.method == "POST":
-        form = AnswerForm(request.POST)
-        if form.is_valid():
-            answer = form.save(commit=False)
-            answer.author = request.user  # author 속성에 로그인 계정 저장
-            answer.create_date = timezone.now()
-            answer.question = question
-            answer.save()
-            return redirect('pybo:detail', question_id=question.id)
-    else:
-        form = AnswerForm()
-    context = {'question': question, 'form': form}
-    return render(request, 'pybo/question_detail.html', context)
+from ..forms import QuestionForm
+from ..models import Question
 
 
 @login_required(login_url='common:login')
@@ -55,6 +21,7 @@ def question_create(request):
         form = QuestionForm()
     context = {'form': form}
     return render(request, 'pybo/question_form.html', context)
+
 
 @login_required(login_url='common:login')
 def question_modify(request, question_id):
@@ -74,6 +41,7 @@ def question_modify(request, question_id):
     context = {'form': form}
     return render(request, 'pybo/question_form.html', context)
 
+
 @login_required(login_url='common:login')
 def question_delete(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -82,3 +50,13 @@ def question_delete(request, question_id):
         return redirect('pybo:detail', question_id=question.id)
     question.delete()
     return redirect('pybo:index')
+
+
+@login_required(login_url='common:login')
+def question_vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user == question.author:
+        messages.error(request, '본인이 작성한 글은 추천할수 없습니다')
+    else:
+        question.voter.add(request.user)
+    return redirect('pybo:detail', question_id=question.id)
